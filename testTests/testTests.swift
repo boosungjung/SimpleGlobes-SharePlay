5a5140f5a68953c0f5c7ae6718b6186e2c949ae7
BooSung Jung <jungboosung01@gmail.com>
2024-07-31 17:19:38 +1000

    Initial Commit

---
+++ testTests/testTests.swift
@@ -0,0 +1,17 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by BooSung Jung on 31/7/2024.
+//
+
+import Testing
+@testable import test
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
